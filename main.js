e3dd7d768f53ff551449b9891f91579e0f022594
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-03-17 17:20:15 -0400

    Added prompts for comments

--- main.js
+++ main.js
@@ -2,6 +2,26 @@ $(() => {
 
     let elementToAdd = $("<h1>Hello, world.</h1>");
     let elementToAddTo = $("#elementToAddTo");
+       
+    /*
+        After adding your event listeners, use your developer tools to see how the HTML changes.
+    */
+
     
-    console.log({elementToAdd, elementToAddTo});
-});
+    // Add a jQuery event listener for #buttonAppend  here
+    // Use .append() to append elementToAdd to elementToAddTo
+
+    // Add a jQuery event listener for #buttonPrepend here
+    // Use .prepend() to prepend elementToAdd to elementToAddTo    
+
+    // Add a jQuery event listener for #buttonBefore  here
+    // Use .before() to add elementToAdd before elementToAddTo
+
+    // Add a jQuery event listener for #buttonAfter here
+    // Use .after() to add elementToAdd after elementToAddTo
+
+    // Add a jQuery event listener for #buttonUpdate here
+    // Use .text
+    // Use .html() to replace #elementToUpdate's inner HTML with <h2>Hello, class!</h2>
+    // Use .text() to insert text into #elementThatNeedsText
+});  
